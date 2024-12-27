016cc8c4d934e4913f12b3a056de95bef23e6873
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2024-12-27 13:28:16 -0300

    Remove node_modules do controle de versao

--- api/fetchMovie.js
+++ api/fetchMovie.js
@@ -1,20 +1,26 @@
 export default async function handler(req, res) {
   const { movie, year } = req.query;
+
+  // Acessa a chave da API usando a variável de ambiente
   const apiKey = process.env.OMDB_API_KEY;
-  const apiUrl = `https://www.omdbapi.com/?apikey=${apiKey}&t=${encodeURIComponent(
-    movie
-  )}${year ? `&y=${year}` : ""}`;
+
+  if (!movie) {
+    return res.status(400).json({ error: "O nome do filme é obrigatório" });
+  }
 
   try {
-    const response = await fetch(apiUrl);
+    const url = `https://www.omdbapi.com/?apikey=${apiKey}&t=${movie}${
+      year ? `&y=${year}` : ""
+    }`;
+    const response = await fetch(url);
     const data = await response.json();
 
     if (data.Error) {
-      return res.status(404).json({ error: "Movie not found" });
+      throw new Error(data.Error);
     }
 
     res.status(200).json(data);
   } catch (error) {
-    res.status(500).json({ error: "Internal server error" });
+    res.status(500).json({ error: error.message });
   }
 }
